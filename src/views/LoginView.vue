5c8de894e9a1736fc7e929048be1a39dcc356033
Aous Mohammad <aous.mohammad97@gmail.com>
2023-01-22 12:39:29 +0300

    add login page

---
+++ src/views/LoginView.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="login">
+        <v-container class="grey lighten-5">
+            <v-row no-gutters>
+                <v-col cols="12" sm="4">
+                    <v-card elevation="2" shaped class="pa-2" outlined tile>
+                        Login
+                        <v-text-field label="Regular"></v-text-field>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LoginView',
+    data: () => ({
+        rules: [
+            value => !!value || 'Required.',
+        ],
+    }),
+}
+</script>
